c4b7f2553264c6a38d46f0cbd353ea6ec85c4c25
+ <omuhindaaaron@gmail.com>
2024-05-23 13:07:36 +0300

    feat: Enter new data in sequences.py

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print("[]")
+    elif length == 1:
+        print("[0]")
+    else:
+        fibonacci_sequence = [0, 1]
+        for _ in range(2, length):
+            next_fib = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+            fibonacci_sequence.append(next_fib)
+        print(f"[{', '.join(map(str, fibonacci_sequence))}]")
+
+
+print_fibonacci(0)  
+print_fibonacci(1)  
+print_fibonacci(2)  
+print_fibonacci(10)
